e7e960af3442937ff09e85c31eb179b4edf44581
Afeef <afeef9706@gmail.com>
2023-08-28 15:43:20 +0800

    Updated rest props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableHighlight } from "react-native";
+import { Text, TouchableHighlight } from "react-native";
 import React from "react";
 
 const Button = ({
@@ -15,13 +15,14 @@ const Button = ({
                 padding: 10,
                 borderRadius: 4,
                 width: "100%",
-                ...props.style,
+                ...props.buttonStyle,
             }}
             {...props}>
             <Text
                 style={{
                     color: textColor,
                     textAlign: "center",
+                    ...props.textStyle,
                 }}>
                 {children}
             </Text>
