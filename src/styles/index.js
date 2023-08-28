f383781753267a55934323e6da7b549bae90f5ae
Afeef <afeef9706@gmail.com>
2023-08-28 15:03:16 +0800

    Created container stylesheet

---
+++ src/styles/index.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const container = StyleSheet.create({
+    main: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    input: {
+        backgroundColor: "white",
+        padding: 10,
+        borderRadius: 4,
+        width: "100%",
+        marginBottom: 4,
+    },
+});
